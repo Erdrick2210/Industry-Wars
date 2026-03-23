90c6f06ddbfa6ea28ec6ed581773e096bad2d2f0
davidal15 <dalopez1503@gmail.com>
2026-03-23 11:58:59 +0100

    First player movement files created

---
+++ scripts/core/player_actions.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+func _physics_process(delta: float) -> void:
+	if(Input.is_action_just_pressed("tecla_w")):
+		pass
+	elif(Input.is_action_just_pressed("tecla_a")):
+		pass
+	elif(Input.is_action_just_pressed("tecla_s")):
+		pass
+	elif(Input.is_action_just_pressed("tecla_d")):
+		pass
