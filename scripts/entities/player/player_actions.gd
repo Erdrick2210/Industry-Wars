3865ed1384f5e471fd5fb7d8ede1f5ee52111b28
davidal15 <dalopez1503@gmail.com>
2026-04-28 13:58:07 +0200

    Level 1 v0.3

---
+++ scripts/entities/player/player_actions.gd
@@ -0,0 +1,55 @@
+extends CharacterBody2D
+
+@export var PlayerAnimation: AnimatedSprite2D
+
+var _walking_speed : float = 120
+var _running_speed : float = _walking_speed * 2
+var last_direction = "down"
+
+# Variable para controlar el estado
+var is_frozen : bool = false 
+
+func _physics_process(delta: float) -> void:
+	# --- LÓGICA DE CONGELAMIENTO ---
+	if is_frozen:
+		velocity = Vector2.ZERO
+		PlayerAnimation.play("idle_" + last_direction)
+		return # <-- "Early Return": Ignora el resto del código si está congelado
+
+	# --- LÓGICA DE MOVIMIENTO ---
+	var current_speed = _walking_speed
+	
+	if (Input.is_action_pressed("shift")):
+		current_speed = _running_speed
+	
+	velocity = Vector2.ZERO
+	
+	if(Input.is_action_pressed("up")):
+		velocity.y = -1
+		last_direction = "up"
+	elif(Input.is_action_pressed("down")):
+		velocity.y = 1
+		last_direction = "down"
+		
+	if(Input.is_action_pressed("left")):
+		velocity.x = -1
+		last_direction = "left"
+	elif(Input.is_action_pressed("right")):
+		velocity.x = 1
+		last_direction = "right"
+	
+	if velocity != Vector2.ZERO:
+		velocity = velocity.normalized() * current_speed
+	
+	move_and_slide()
+	
+	# --- LÓGICA DE ANIMACIÓN ---
+	if(velocity != Vector2.ZERO):
+		PlayerAnimation.speed_scale = 2 if current_speed == _running_speed else 1.0
+		PlayerAnimation.play("run_" + last_direction)
+	else:
+		PlayerAnimation.play("idle_" + last_direction)
+
+# Función pública para que el NPC pueda llamarla
+func set_frozen(value: bool):
+	is_frozen = value
