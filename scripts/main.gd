9fcf7dbc402b543c6544f94d810e2edc58a6cb22
davidal15 <dalopez1503@gmail.com>
2026-05-13 04:05:52 +0200

    Pause menu fixed

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,6 +7,7 @@ var _instantiated_level : Node
 var _level_cache : Dictionary = {}
 
 func _ready() -> void:
+	
 	GameEvents.change_level_request.connect(_on_level_change_requested)
 	# Usamos la misma función de cambio para el nivel 1 para que se guarde en cache
 	_change_level("res://game/levels/level_1/playerHome.tscn")
@@ -33,3 +34,23 @@ func _change_level(level_path: String):
 	
 	if _instantiated_level.has_method("prepare_level"):
 		_instantiated_level.prepare_level()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		_toggle_bag()
+
+func _toggle_bag() -> void:
+	# Si ya está abierto, cierra el CanvasLayer
+	for child in get_children():
+		if child.name == "MenuCanvas":
+			child.queue_free()
+			return
+	
+	# Si no está abierto, lo crea
+	var menu = preload("res://game/scenes/inventory/MainMenu.tscn").instantiate()
+	var canvas = CanvasLayer.new()
+	canvas.name = "MenuCanvas"
+	add_child(canvas)
+	canvas.add_child(menu)
+	menu.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
