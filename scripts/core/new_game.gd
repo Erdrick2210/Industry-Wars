4acaed78ccde8f57f7c90a9de8317fe3ba3aaa5a
Randyy27 <joanrandy2707@gmail.com>
2026-05-26 22:31:39 +0200

    tutorial

--- scripts/core/new_game.gd
+++ scripts/core/new_game.gd
@@ -2,4 +2,4 @@ extends Button
 
 func _on_pressed() -> void:
 	GameEvents.emit_signal("change_level_request", "res://game/levels/level_1/playerHome.tscn", "DefaultSpawn")
-  AudioManager.play_sfx("res://assets/audio/sfx/select.WAV")
+	AudioManager.play_sfx("res://assets/audio/sfx/select.WAV")
