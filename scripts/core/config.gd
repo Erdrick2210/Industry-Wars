be8b355d1cd17c8e198fbd2707af82f18231d6dc
Erdrick2210 <ericrode22@gmail.com>
2026-05-27 01:38:20 +0200

    hotfix + new game erases save_files

--- scripts/core/config.gd
+++ scripts/core/config.gd
@@ -5,4 +5,7 @@ const OPTIONS_MENU = preload("res://game/scenes/options_menu.tscn")
 func _on_pressed() -> void:
 	AudioManager.play_sfx("res://assets/audio/sfx/select.WAV")
 	var options = OPTIONS_MENU.instantiate()
-	get_tree().current_scene.add_child(options)
+	var canvas = CanvasLayer.new()
+	get_tree().current_scene.add_child(canvas)
+	canvas.add_child(options)
+	options.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
--- scripts/core/new_game.gd
+++ scripts/core/new_game.gd
@@ -1,5 +1,13 @@
 extends Button
 
+var files = [
+		"user://save_game.cfg",
+		"user://save_game.json"
+	]
+	
 func _on_pressed() -> void:
+	for path in files:
+		if FileAccess.file_exists(path):
+			DirAccess.remove_absolute(path)
 	GameEvents.emit_signal("change_level_request", "res://game/levels/level_1/playerHome.tscn", "DefaultSpawn")
 	AudioManager.play_sfx("res://assets/audio/sfx/select.WAV")
